2894264e9a88384b14c8eead5409204a26ec0554
DanielRasho <ray22933@uvg.edu.gt>
2024-07-27 23:07:29 -0600

    feat: fix import error for inputField component

--- src/pages/admin/UsersView.vue
+++ src/pages/admin/UsersView.vue
@@ -26,7 +26,7 @@
 import { ref, onMounted } from 'vue'
 import '@/assets/arrow-left-double-fill.svg'
 import Button from '@/components/Buttons/ButtonSimple.vue'
-import CustomInput from '@/components/Forms/InputField/CustomInput.vue'
+import CustomInput from '@/components/Forms/InputField/InputField.vue'
 import ViewUser from '@/pages/admin/ViewUser.vue'
 import DisplayTable from '@/components/DataDisplay/Tables/DisplayTable.vue'
 
