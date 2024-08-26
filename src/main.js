cf0dd504d470f6bdcb5ab15a863f00676b67d757
DanielRasho <ray22933@uvg.edu.gt>
2024-08-26 12:38:13 -0600

    feat: export auth0 client object on its own file

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/styles/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import { createAuth0 } from '@auth0/auth0-vue'
+import { authClient } from './oauth'
 
 import App from './App.vue'
 import router from './router'
@@ -12,15 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(
-  createAuth0({
-    domain: import.meta.env.VITE_OAUTH_DOMAIN,
-    clientId: import.meta.env.VITE_OAUTH_CLIENT_ID,
-    authorizationParams: {
-      redirect_uri: import.meta.env.VITE_OAUTH_REDIRECT_URI
-    },
-    useRefreshTokens: true
-  })
-)
+app.use(authClient)
 
 app.mount('#app')
