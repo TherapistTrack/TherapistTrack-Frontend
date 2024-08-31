7c9558d2c183b6168ce7158bb67a64a1875079e2
MarchMol <marchmol98@gmail.com>
2024-08-31 00:51:35 -0600

    feat: global spinner component

---
+++ src/components/Feedback/Spinner/OverlayLoader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="overlay-loader">
+    <RiLoader2Line size="3rem" color="black" alt="loading" class="loading" />
+  </div>
+</template>
+
+<script setup>
+import { RiLoader2Line } from '@remixicon/vue'
+</script>
+
+<style>
+.overlay-loader {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.2);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 500;
+}
+
+.loading {
+  height: 12vh;
+  animation: rotate 2.5s linear infinite;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
