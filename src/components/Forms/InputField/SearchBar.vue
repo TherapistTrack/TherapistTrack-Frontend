3d375525d444e2736409ed5cd982ac497f220244
MarchMol <marchmol98@gmail.com>
2024-09-09 05:07:33 -0600

    fix: search bar UI and functionality

--- src/components/Forms/InputField/SearchBar.vue
+++ src/components/Forms/InputField/SearchBar.vue
@@ -1,14 +1,59 @@
 <template>
   <div class="searchBar">
-    <input :placeholder="pholder" /><RiSearchLine size="1rem" color="var(--gray-1)" alt="search" />
+    <RiSearchLine size="1rem" color="var(--gray-1)" alt="search" />
+    <input :placeholder="pholder" :value="searchValue" @input="updateValue" />
+
+    <div class="container" :id="isEmpty ? '' : 'active'">
+      <RiCloseCircleLine
+        v-if="showIcon"
+        size="1rem"
+        color="var(--gray-1)"
+        class="close"
+        @click="empty"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { RiSearchLine } from '@remixicon/vue'
+import { RiSearchLine, RiCloseCircleLine } from '@remixicon/vue'
+import { ref, watch } from 'vue'
+const isEmpty = ref(true)
+const showIcon = ref(false)
 defineProps({
-  pholder: String
+  pholder: String,
+  searchValue: String
 })
+
+watch(isEmpty, async () => {
+  if (isEmpty.value == false) {
+    setTimeout(() => {
+      showIcon.value = true
+    }, 300)
+  } else {
+    showIcon.value = false
+  }
+})
+
+const updateLol = (val) => {
+  if (val == '') {
+    isEmpty.value = true
+  } else {
+    isEmpty.value = false
+  }
+}
+
+const empty = () => {
+  emit('update:searchValue', '')
+  isEmpty.value = true
+}
+
+const emit = defineEmits(['update:searchValue'])
+
+const updateValue = (event) => {
+  emit('update:searchValue', event.target.value)
+  updateLol(event.target.value)
+}
 </script>
 
 <style scoped>
@@ -21,6 +66,7 @@ defineProps({
   border-radius: 2vh;
   padding: 1vh;
   color: #6e6e6e;
+  gap: 1rem;
 }
 
 .searchBar input {
@@ -31,5 +77,24 @@ defineProps({
 
 .searchBar img {
   height: 3vh;
+}
+
+.searchBar .container {
+  width: 0;
+  display: flex;
+  position: relative;
+  transition: width 0.5s;
+}
+.searchBar .container#active {
+  width: 1rem;
+}
+
+.searchBar .close {
+  position: absolute;
+  top: -0.5rem;
+}
+
+.searchBar .close:hover {
+  fill: var(--gray-2);
 }
 </style>
