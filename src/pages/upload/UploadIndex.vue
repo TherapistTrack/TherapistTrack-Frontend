40d19bd553b719ba5dc1401279691f4b2fb2264e
MarchMol <marchmol98@gmail.com>
2024-11-16 20:26:58 -0600

    feat: upload now connected to endpoint

---
+++ src/pages/upload/UploadIndex.vue
@@ -0,0 +1,52 @@
+<template>
+  <router-view
+    :doctorId="doctorId"
+    :recordId="recordId"
+    @goToSelect="goToSelect"
+    @goToForm="goToForm"
+    @goToFiles="goToFiles"
+    @goToDoing="goToDoing"
+    @goToFinish="goToFinish"
+    @goToStart="returnToStart"
+  />
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useUploadStore } from '@/stores/uploadStore'
+
+const uploadStore = useUploadStore()
+const router = useRouter()
+
+onMounted(() => {
+  uploadStore.files = []
+  goToFiles()
+  console.log(uploadStore.recordId, uploadStore.doctorId)
+})
+
+const goToFiles = () => {
+  router.push(`/upload/files`)
+}
+
+const goToSelect = () => {
+  router.push(`/upload/select`)
+}
+const goToForm = () => {
+  router.push(`/upload/form`)
+}
+
+const goToDoing = () => {
+  router.push(`/upload/doing`)
+}
+
+const goToFinish = () => {
+  router.push(`/upload/finish`)
+}
+
+const returnToStart = () => {
+  router.push('/doctor')
+}
+</script>
+
+<style scoped></style>
